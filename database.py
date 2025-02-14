d4f7e6973a2b6e7703b062366a0531b6a2706043
Dagmar87 <jdfssobrinho@hotmail.com>
2025-02-14 12:42:41 -0300

    Criação da versão pdf do Swagger UI e do arquivo .env e ajustes nos dados do banco de dados no arquivo database.py

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-SQLALCHEMY_DATABASE_URL = 'sqlite:///./empresas.db'
+SQLALCHEMY_DATABASE_URL = 'postgresql+asyncpg://USER:PASSWORD@localhost:6432/DBNAME'
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={'check_same_thread': False})
 
