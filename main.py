74454cde888661f3eeb12669b4a5183ab32fb070
Dagmar87 <jdfssobrinho@hotmail.com>
2025-02-14 11:45:18 -0300

    Desenvolvimento do codigo do arquivo main.py (Criação das operações crud referente a Obrigação Acessória)

--- main.py
+++ main.py
@@ -32,6 +32,7 @@ class ObrigacaoAcessoriaRequest(BaseModel):
     periodicidade: str = Field(min_length=1, max_length=255)
     empresa_id: int
 
+# Empresa
 # Listar todas as empresas
 @app.get('/empresas', status_code=status.HTTP_200_OK)
 async def read_all_empresas(db: db_dependency):
@@ -74,4 +75,49 @@ async def delete_empresa(db: db_dependency, empresa_id: int = Path(gt=0)):
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='Empresa não encontrada')
     
     db.delete(empresa)
+    db.commit()
+
+#  Obrigação Acessória 
+# Listar todas obrigações acessórias 
+@app.get('/obrigacaoAcessoria', status_code=status.HTTP_200_OK)
+async def read_all_obrigacaoAcessoria(db: db_dependency):
+    return db.query(ObrigacaoAcessoria).all()
+    
+# Procurar uma obrigação acessória especifica pelo id
+@app.get('/obrigacaoAcessoria/{obrigacaoAcessoria_id}', status_code=status.HTTP_200_OK)
+async def get_obrigacaoAcessoria_by_id(db: db_dependency, obrigacaoAcessoria_id: int = Path(gt=0)):
+    obrigacaoAcessoria = db.query(ObrigacaoAcessoria).filter(ObrigacaoAcessoria.id == obrigacaoAcessoria_id).first()
+    if obrigacaoAcessoria is not None:
+        return obrigacaoAcessoria
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='Obrigação Acessória não encontrada')
+    
+# Criar uma nova obrigação acessória
+@app.post('/obrigacaoAcessoria', status_code=status.HTTP_201_CREATED)
+async def create_obrigacaoAcessoria(db: db_dependency, obrigacaoAcessoria_request: ObrigacaoAcessoriaRequest):
+    new_obrigacaoAcessoria = ObrigacaoAcessoria(**obrigacaoAcessoria_request.model_dump())
+
+    db.add(new_obrigacaoAcessoria)
+    db.commit()
+    
+# Editar uma obrigação acessória existente
+@app.put('/obrigacaoAcessoria/{obrigacaoAcessoria_id}', status_code=status.HTTP_204_NO_CONTENT)
+async def update_obrigacaoAcessoria(db: db_dependency, obrigacaoAcessoria_request: ObrigacaoAcessoriaRequest, obrigacaoAcessoria_id: int = Path(gt=0)):
+    obrigacaoAcessoria = db.query(ObrigacaoAcessoria).filter(ObrigacaoAcessoria.id == obrigacaoAcessoria_id).first()
+    if obrigacaoAcessoria is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='Obrigação Acessória não encontrada')
+    
+    for var, value in vars(obrigacaoAcessoria_request).items():
+        setattr(obrigacaoAcessoria, var, value) if value else None
+
+    db.commit()
+
+# Excluir uma obrigação acessória
+@app.delete('/obrigacaoAcessoria/{obrigacaoAcessoria_id}', status_code=status.HTTP_204_NO_CONTENT)
+async def delete_obrigacaoAcessoria(db: db_dependency, obrigacaoAcessoria_id: int = Path(gt=0)):
+    obrigacaoAcessoria = db.query(ObrigacaoAcessoria).filter(ObrigacaoAcessoria.id == obrigacaoAcessoria_id).first()
+
+    if obrigacaoAcessoria is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='Obrigação Acessória não encontrada')
+    
+    db.delete(obrigacaoAcessoria)
     db.commit()
