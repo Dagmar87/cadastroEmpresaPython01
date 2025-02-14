39a19b173e0ff646438417a18ccf7cfc68b3ead1
Dagmar87 <jdfssobrinho@hotmail.com>
2025-02-14 11:19:02 -0300

    Desenvolvimento do codigo do arquivo main.py (Criação das operações crud referente a Empresa)

--- main.py
+++ main.py
@@ -29,4 +29,49 @@ class EmpresaRequest(BaseModel):
 
 class ObrigacaoAcessoriaRequest(BaseModel):
     nome: str = Field(min_length=1, max_length=255)
-    periodicidade: str(min_length=1, max_length=255)
+    periodicidade: str = Field(min_length=1, max_length=255)
+    empresa_id: int
+
+# Listar todas as empresas
+@app.get('/empresas', status_code=status.HTTP_200_OK)
+async def read_all_empresas(db: db_dependency):
+    return db.query(Empresa).all()
+
+# Procurar uma empresa especifica pelo id
+@app.get('/empresa/{empresa_id}', status_code=status.HTTP_200_OK)
+async def get_empresa_by_id(db: db_dependency, empresa_id: int = Path(gt=0)):
+    empresa = db.query(Empresa).filter(Empresa.id == empresa_id).first()
+    if empresa is not None:
+        return empresa
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='Empresa não encontrada')
+
+# Criar uma nova empresa
+@app.post('/empresa', status_code=status.HTTP_201_CREATED)
+async def create_empresa(db: db_dependency, empresa_request: EmpresaRequest):
+    new_empresa = Empresa(**empresa_request.model_dump())
+
+    db.add(new_empresa)
+    db.commit()
+
+# Editar uma empresa existente
+@app.put('/empresa/{empresa_id}', status_code=status.HTTP_204_NO_CONTENT)
+async def update_empresa(db: db_dependency, empresa_request: EmpresaRequest, empresa_id: int = Path(gt=0)):
+    empresa = db.query(Empresa).filter(Empresa.id == empresa_id).first()
+    if empresa is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='Empresa não encontrada')    
+    
+    for var, value in vars(empresa_request).items():
+        setattr(empresa, var, value) if value else None
+
+    db.commit()
+
+# Excluir uma empresa
+@app.delete('/empresa/{empresa_id}', status_code=status.HTTP_204_NO_CONTENT)
+async def delete_empresa(db: db_dependency, empresa_id: int = Path(gt=0)):
+    empresa = db.query(Empresa).filter(Empresa.id == empresa_id).first()
+
+    if empresa is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='Empresa não encontrada')
+    
+    db.delete(empresa)
+    db.commit()
